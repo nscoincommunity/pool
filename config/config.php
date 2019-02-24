997cd8dbdad302a27ca9cba0344843823cd431da
originchainio <46016927+originchainio@users.noreply.github.com>
2019-02-24 22:24:37 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -13,7 +13,9 @@ return array_merge(array(
 	'address'=>'',
 
 	'min_pay'=>100,
-
+	// If you configure PHP environment variables and allow them to be invoked globally directly by PHP CGI mode, you can leave them blank. If it is a quick installation script install, please keep it unchanged
+	'php_path'=>'/usr/local/php/bin/',
+	
 ),include(__DIR__.'/config_db.php'));
 
 ?>
