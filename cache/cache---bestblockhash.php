684d551d1a936b17c5a73f89e08ac5ac69592da4
originchainio <46016927+originchainio@users.noreply.github.com>
2019-02-25 15:41:16 +0800

    Delete cache---bestblockhash.php

--- cache/cache---bestblockhash.php
+++
@@ -1,8 +0,0 @@
-<?php
- // mktime: 1550406950
- return array (
-  'contents' => '3nLpnwpdy7M3VeMeimDyS9LbuJVvCv6nXmsioVxvYWVnhoUYTTcfuHMGNfSsd3aRNVDpaBaVq5dV8c9HnpobrkHu',
-  'expire' => 0,
-  'mtime' => 1550406950,
-)
-?>
